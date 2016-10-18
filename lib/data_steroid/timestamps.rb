a206cf380080472d95bf410e25baaf409ce1dee4
Giovanni Bonetti <giovanni.bonetti@gmail.com>
2016-10-18 15:56:57 -0200

    update_updated_at -> set_updated_at

--- lib/data_steroid/timestamps.rb
+++ lib/data_steroid/timestamps.rb
@@ -9,9 +9,9 @@ module DataSteroid
       property :created_at, default: DateTime.now
       property :updated_at
 
-      before_save :update_updated_at
+      before_save :set_updated_at
 
-      def update_updated_at
+      def set_updated_at
         self.updated_at = DateTime.now
       end
     end
