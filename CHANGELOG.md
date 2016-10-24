2a4991f04a66a069f9cc1b770e01abcc61ada0c3
Giovanni Bonetti <giovanni.bonetti@gmail.com>
2016-10-24 11:52:52 -0200

    trocando DateTime por Time

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Change Log
 
+## 0.5.2 (2016-10-24)
+- Switched timestamps from DateTime to Time for simpler usage with gRPC int32. Nowadays [Time has most capabilities of Datetime anyway](http://stackoverflow.com/questions/1261329/whats-the-difference-between-datetime-and-time-in-ruby#answer-1261435)
+
 ## 0.5.1 (2016-10-19)
 - Added coercion to Datastore types (on save)
 
@@ -11,4 +14,3 @@
 
 ## 0.4.2 (2016-10-18)
 - Added 'require time' to Timestamps module
-
--- lib/data_steroid/timestamps.rb
+++ lib/data_steroid/timestamps.rb
@@ -1,18 +1,16 @@
-require 'time'
-
 module DataSteroid
   # Define behaviour to created_at and updated_at data
   module Timestamps
     extend ActiveSupport::Concern
 
     included do
-      property :created_at, default: ->{ DateTime.now }
+      property :created_at, default: ->{ Time.now }
       property :updated_at
 
       before_save :set_updated_at
 
       def set_updated_at
-        self.updated_at = DateTime.now
+        self.updated_at = Time.now
       end
     end
   end
