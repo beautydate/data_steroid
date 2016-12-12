45a156eb8d1feeeddb2ae93fe36d61d9190f79bb
Marco Carvalho <marco.carvalho.swasthya@gmail.com>
2016-12-12 17:43:25 -0200

    find by id

--- spec/persist_spec.rb
+++ spec/persist_spec.rb
@@ -9,20 +9,40 @@ class ProductTest
 end
 
 RSpec.describe ProductTest, type: :model do
+  let(:product) { ProductTest.new(name: 'iPhone XYZ', price: 1.2) }
   before :each do
     # TODO: clear database before suite
     clear_datastore_kind(ProductTest)
   end
 
   it 'Save' do
-    p = ProductTest.new(name: 'iPhone XYZ', price: 1.2)
-    expect(p.save).to be_truthy
+    expect(product.save).to be_truthy
 
     res = ProductTest.fetch ProductTest.query.where('name', '=', 'iPhone XYZ')
     expect(res.count).to eq(1)
 
     p1 = res.first
-    expect(p1.name).to eq(p.name)
-    expect(p1.price).to eq(p.price)
+    expect(p1.name).to eq(product.name)
+    expect(p1.price).to eq(product.price)
+  end
+
+  it 'delete' do
+    product.save
+
+    res = ProductTest.fetch ProductTest.query.where('name', '=', 'iPhone XYZ')
+    expect(res.count).to eq(1)
+
+    product.delete
+
+    res = ProductTest.fetch ProductTest.query.where('name', '=', 'iPhone XYZ')
+    expect(res.count).to eq(0)
+  end
+
+  it 'find by id' do
+    product.save
+
+    p = ProductTest.find(product.id)
+
+    expect(p.name).to eq(product.name)
   end
 end
